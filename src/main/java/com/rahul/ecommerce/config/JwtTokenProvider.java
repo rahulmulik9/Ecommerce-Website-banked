1a15a219602c4ec0a165573df8fa19f6ac0a8fda
Rahul Mulik <rahulmulik9@gmail.com>
2024-03-21 22:10:01 +0530

    JwtTokenProvider has been added. Using this we can extract email and authorities

--- src/main/java/com/rahul/ecommerce/config/JwtTokenProvider.java
+++ src/main/java/com/rahul/ecommerce/config/JwtTokenProvider.java
@@ -1,44 +1,47 @@
 package com.rahul.ecommerce.config;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.stereotype.Service;
-
-import javax.crypto.SecretKey;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.crypto.SecretKey;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
 @Service
 public class JwtTokenProvider {
-	
-	private SecretKey key=Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
-	
+
+	private SecretKey theSecreteKey = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+
 	public String generateToken(Authentication auth) {
 		String jwt = Jwts.builder()
 				.issuedAt(new Date())
-				.expiration(new Date(new Date().getTime()+86400000))
+				.expiration(new Date(new Date().getTime()+864000000))
 				.claim("email",auth.getName())
-				.signWith(key)
+				.signWith(theSecreteKey)
 				.compact();
-		
-		return jwt;	
+
+		return jwt;
 	}
-	
+
 	public String getEmailFromJwtToken(String jwt) {
 		jwt=jwt.substring(7);
-		Claims claims= Jwts.parser().decryptWith(key).build().parseSignedClaims(jwt).getPayload();
+		Claims claims= Jwts.parser().decryptWith(theSecreteKey).build().parseSignedClaims(jwt).getPayload();
 		String email=String.valueOf(claims.get("email"));
 		return email;
 	}
-	
+
 	public String populateAuthorities(Collection<? extends GrantedAuthority> collection) {
 		Set<String> auths=new HashSet<>();
-		
+
 		for(GrantedAuthority authority:collection) {
 			auths.add(authority.getAuthority());
 		}
