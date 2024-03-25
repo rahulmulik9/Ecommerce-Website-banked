b6db053f9cde091fb37557abd4a5fc0f3a89af91
Rahul Mulik <rahulmulik9@gmail.com>
2024-03-25 12:05:17 -0400

    Added Controller

---
+++ src/main/java/com/rahul/ecommerce/controller/CartController.java
@@ -0,0 +1,55 @@
+package com.rahul.ecommerce.controller;
+
+import com.rahul.ecommerce.exception.ProductException;
+import com.rahul.ecommerce.exception.UserException;
+import com.rahul.ecommerce.modal.Cart;
+import com.rahul.ecommerce.modal.CartItem;
+import com.rahul.ecommerce.modal.User;
+import com.rahul.ecommerce.request.AddItemRequest;
+import com.rahul.ecommerce.response.ApiResponse;
+import com.rahul.ecommerce.service.CartService;
+import com.rahul.ecommerce.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/cart")
+public class CartController {
+	
+	private CartService cartService;
+	private UserService userService;
+	
+	public CartController(CartService cartService,UserService userService) {
+		this.cartService=cartService;
+		this.userService=userService;
+	}
+	
+	@GetMapping("/")
+	public ResponseEntity<Cart> findUserCartHandler(@RequestHeader("Authorization") String jwt) throws UserException{
+		
+		User user=userService.findUserProfileByJwt(jwt);
+		
+		Cart cart=cartService.findUserCart(user.getId());
+		
+		System.out.println("cart - "+cart.getUser().getEmail());
+		
+		return new ResponseEntity<Cart>(cart,HttpStatus.OK);
+	}
+	
+	@PutMapping("/add")
+	public ResponseEntity<CartItem> addItemToCart(@RequestBody AddItemRequest req, 
+			@RequestHeader("Authorization") String jwt) throws UserException, ProductException{
+		
+		User user=userService.findUserProfileByJwt(jwt);
+		
+		CartItem item = cartService.addCartItem(user.getId(), req);
+		
+		ApiResponse res= new ApiResponse("Item Added To Cart Successfully",true);
+		
+		return new ResponseEntity<>(item,HttpStatus.ACCEPTED);
+		
+	}
+	
+
+}
